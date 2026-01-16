cfe5011b7a37fb5d4fff6ca944386acf7c2997a8
AaronDLF <aaron10lopfortich@gmail.com>
2026-01-15 22:31:17 -0500

    Add the first objects on the surface

--- plot.c
+++ plot.c
@@ -1,17 +1,23 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_video.h>
 #include <stdbool.h>
 #include <stdio.h>
 
 #define WIDTH 900
 #define HEIGHT 600
+#define COLOR 0xFF0000
 
 int main() {
   printf("Welcome to the function plotter!\n");
   SDL_Window *pwindow =
       SDL_CreateWindow("Function Plotter", SDL_WINDOWPOS_CENTERED,
                        SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
-  SDL_Delay(3000);
+
+  SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
+
+  SDL_Rect rect = (SDL_Rect){50, 50, 50, 50};
+  SDL_FillRect(psurface, &rect, COLOR);
 
   SDL_Event event;
   bool app_running = true;
@@ -23,5 +29,6 @@ int main() {
         app_running = false;
       }
     }
+    SDL_UpdateWindowSurface(pwindow);
   }
 }
